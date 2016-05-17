cc49fbe6fa18cffcdf8525c0ef03cc485cd2cdb7
hcq <hcq20160303@aliyun.com>
2016-05-17 18:57:24 +0800

    add the conf files

---
+++ rddShare/rddShare.sql
@@ -0,0 +1,46 @@
+-- MySQL dump 10.13  Distrib 5.6.30, for Linux (x86_64)
+--
+-- Host: localhost    Database: rddShare
+-- ------------------------------------------------------
+-- Server version	5.6.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `rddShare`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `rddShare` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `rddShare`;
+
+--
+-- Table structure for table `repository`
+--
+
+DROP TABLE IF EXISTS `repository`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `repository` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nodesList` text,
+  `indexOfDagScan` text,
+  `outputFileLastModifiedTime` bigint(20) DEFAULT NULL,
+  `sizeOfOutputData` double DEFAULT NULL,
+  `exeTimeOfDag` bigint(20) DEFAULT NULL,
+  `reuse` int(11) DEFAULT NULL,
+  `outputFilename` text,
+  `insertTime` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
